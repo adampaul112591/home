328c70afbd2851e15302972d97f492bbbb93a2f9
adampaul <adampaul@indiana.edu>
2013-09-06 14:12:07 -0400

    hw2
    download

---
+++ flood.py
@@ -0,0 +1,4 @@
+from utilities import *
+
+def flood(color_of_tile, flooded_list, screen_size):
+    pass
---
+++ plotter.py
@@ -0,0 +1,19 @@
+import csv
+import matplotlib.pyplot as plt
+import math
+
+xvals = []
+yvals = []
+# read csv
+with open('times.csv', 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+#         print row
+        xvals.append(row[0]); yvals.append(row[1])
+
+# plot the times
+plt.xlabel('#tiles'); plt.ylabel('Time')
+xpts = [math.sqrt(float(xval)) for xval in xvals]
+# plt.xticks(xpts, [str(sz) for sz in xpts])
+plt.plot(xvals, yvals, 'bo-')
+plt.show()
